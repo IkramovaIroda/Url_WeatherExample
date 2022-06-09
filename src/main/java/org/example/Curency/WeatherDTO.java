335b3926808489b959f6a39c63d3df520eb274c3
Lenovo PC <ikramovairoda1311@gmail.com>
2022-06-09 15:50:15 +0300

    commit

--- src/main/java/org/example/Curency/WeatherDTO.java
+++ src/main/java/org/example/Curency/WeatherDTO.java
@@ -1,12 +1,12 @@
 package org.example.Curency;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.example.Curency.dto.Clouds;
 import org.example.Curency.dto.Main;
-import org.example.Curency.dto.Sys;
 import org.example.Curency.dto.Wind;
 
 @Getter
@@ -14,19 +14,29 @@ import org.example.Curency.dto.Wind;
 @NoArgsConstructor
 @AllArgsConstructor
     public class WeatherDTO {
+//    @SerializedName("coord")
         private org.example.Curency.dto.coord coord;
+ //   @SerializedName("weather")
         private org.example.Curency.dto.weather weather;
+  //  @SerializedName("base")
         private String base;
+   // @SerializedName("main")
         private Main main;
+   // @SerializedName("visibility")
     private int visibility;
+   // @SerializedName("wind")
     private Wind wind;
+   // @SerializedName("clouds")
     private Clouds clouds;
-
+   // @SerializedName("dt")
     private long dt;
-    private Sys sys;
+//    @SerializedName("timezone")
     private long timezone;
+   // @SerializedName("id")
     private long id;
+   // @SerializedName("name")
     private String name;
+   // @SerializedName("cod")
     private int cod;
 
     }
--- src/main/java/org/example/Curency/WeatherMain.java
+++ src/main/java/org/example/Curency/WeatherMain.java
@@ -1,14 +1,20 @@
 package org.example.Curency;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
 
 public class WeatherMain {
+  static   JsonParser parser = new JsonParser();
     public static void main(String[] args) throws IOException {
     URL url = new URL("https://api.openweathermap.org/data/2.5/weather?lat=41.311081&lon=69.240562&appid=c1bfbf2e4b102a8e72480b3754321694");
     URLConnection connection = url.openConnection();
@@ -21,18 +27,33 @@ public class WeatherMain {
         stringBuilder.append(line);
         line = bufferedReader.readLine();
     }
-//    Gson gson = new Gson();
-//    CurrensyDTO currensyDTO = gson.fromJson(stringBuilder.toString(), CurrensyDTO.class);
-//        System.out.println("id: " + currensyDTO.getId());
-//        System.out.println("Code: " + currensyDTO.getBase());
-//        System.out.println("Ccy: " + currensyDTO.getMain());
-//        System.out.println("CcyNm_RU: " + currensyDTO.getClouds());
-//        System.out.println("CcyNm_UZ: " + currensyDTO.getSys());
-//        System.out.println("CcyNm_UZC: " + currensyDTO.getVisibility());
-//        System.out.println("CcyNm_EN: " + currensyDTO.getWind());
-//        System.out.println("Nominal: " + currensyDTO.getClouds());
-//        System.out.println("Rate: " + currensyDTO.getWeather());
-//        System.out.println("Diff: " + currensyDTO.getCod());
-//        System.out.println("Date: " + currensyDTO.getDt());
+   Gson gson = new Gson();
+
+// 1 способ
+        JsonObject jo =parser.parse(stringBuilder.toString()).getAsJsonObject();
+        JsonObject location = jo.get("location").getAsJsonObject();
+        JsonObject current = jo.get("current").getAsJsonObject();
+        String cityName = location.get("name").getAsString();
+        Double tempC = current.get("temp_c").getAsDouble();
+
+
+//        WeatherDTO weatherDTO = gson.fromJson(stringBuilder.toString() , WeatherDTO.class);
+//        System.out.println(weatherDTO);
+
+//2 способ
+        //   WeatherDTO weatherDTO = gson.<WeatherDTO>fromJson(stringBuilder.toString(), WeatherDTO.class);
+       // System.out.println(" " + weatherDTO.getClass());
+//        System.out.println("" + weatherDTO.getBase());
+//        System.out.println("" + weatherDTO.getMain());
+//        System.out.println(" " + weatherDTO.getClouds());
+//        System.out.println("" + weatherDTO.getSys());
+//        System.out.println("" + weatherDTO.getVisibility());
+//        System.out.println("" + weatherDTO.getWind());
+//        System.out.println("" + weatherDTO.getClouds());
+//        System.out.println("" + weatherDTO.getWeather());
+//        System.out.println("" + weatherDTO.getCod());
+//        System.out.println("" + weatherDTO.getDt());
+//3 способ
+        //List<WeatherDTO> weatherDTO = (List<WeatherDTO>) new Gson().fromJson( stringBuilder.toString() , WeatherDTO.class);
 }
 }
